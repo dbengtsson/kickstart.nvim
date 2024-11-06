6bc3d1e753cf0984fb707fe22bfb267191683d23
Daniel Bengtsson <dpbengtsson@gmail.com>
2024-11-06 21:36:07 +0100

    added c# lsp

--- init.lua
+++ init.lua
@@ -618,6 +618,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
+        csharp_ls = {},
         ts_ls = {},
         prettierd = {},
         eslint = {},
